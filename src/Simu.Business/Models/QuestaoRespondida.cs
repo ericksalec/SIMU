245e9ce719d427c80e47fe4efd7379d258065375
Erick Sales <ericksalec@gmail.com>
2021-11-16 14:10:03 -0300

    Adicionando lista de questões.

---
+++ src/Simu.Business/Models/QuestaoRespondida.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simu.Business.Models
+{
+    public class QuestaoRespondida : Entity
+    {
+        public Guid TarefaId { get; set; }
+        public Guid UserId { get; set; }
+        public string Prova { get; set; }
+        public string Enunciado { get; set; }
+        public string TipoAssunto { get; set; }
+        public string Resposta { get; set; }
+        public string RespostaMarcada { get; set; }
+        public bool Acerto { get; set; }
+        public int AnoProva { get; set; }
+        public string A { get; set; }
+        public string B { get; set; }
+        public string C { get; set; }
+        public string D { get; set; }
+
+    }
+}
